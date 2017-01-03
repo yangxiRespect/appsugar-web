6156472a08b5721207b2e06e1585fd8ac2a4a5c3
shenliuyang <shenliuyang@gmail.com>
2017-01-03 14:48:44 +0800

    upgrade solr version

--- appsugar-data-solr/src/main/java/org/appsugar/data/solr/SolrIdEntityRepositoryImpl.java
+++ appsugar-data-solr/src/main/java/org/appsugar/data/solr/SolrIdEntityRepositoryImpl.java
@@ -9,10 +9,28 @@ import org.appsugar.bean.domain.Sort;
 import org.appsugar.bean.entity.StringIdEntity;
 import org.appsugar.data.common.repository.ext.RepositoryExtension;
 import org.appsugar.data.common.repository.ext.RepositoryExtensionable;
+import org.springframework.data.solr.core.SolrOperations;
+import org.springframework.data.solr.repository.query.SolrEntityInformation;
 import org.springframework.data.solr.repository.support.SimpleSolrRepository;
 
 public class SolrIdEntityRepositoryImpl<T extends StringIdEntity, C extends StringIdEntityCondition> extends
 		SimpleSolrRepository<T, String> implements RepositoryExtensionable<String, T, C>, SolrIdEntityRepository<T, C> {
+
+	public SolrIdEntityRepositoryImpl() {
+		super();
+	}
+
+	public SolrIdEntityRepositoryImpl(SolrEntityInformation<T, ?> metadata, SolrOperations solrOperations) {
+		super(metadata, solrOperations);
+	}
+
+	public SolrIdEntityRepositoryImpl(SolrOperations solrOperations, Class<T> entityClass) {
+		super(solrOperations, entityClass);
+	}
+
+	public SolrIdEntityRepositoryImpl(SolrOperations solrOperations) {
+		super(solrOperations);
+	}
 
 	@Override
 	public void setRepositoryExtension(RepositoryExtension<String, T, C> repositoryExtension) {
